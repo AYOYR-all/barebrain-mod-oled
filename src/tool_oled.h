c6242cdc31e5b23bf9492fe9f237ccc591e4c570
AYOYR-all <barajasdavies986@gmail.com>
2026-06-26 17:39:46 +0800

    Add SSD1306 OLED plugin

--- src/tool_oled.h
+++ src/tool_oled.h
@@ -1,8 +1,12 @@
 #pragma once
 
 #include <stddef.h>
+#include <stdbool.h>
 
 #include "esp_err.h"
 
 esp_err_t tool_oled_init(void);
 esp_err_t tool_oled_execute(const char *input_json, char *output, size_t output_size);
+esp_err_t tool_oled_set_clock_enabled(bool enabled);
+esp_err_t tool_oled_draw_clock(void);
+esp_err_t tool_oled_draw_face(const char *emotion, unsigned int frame);
